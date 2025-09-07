80fd31e81e08ede4e3192518160c24fc8eaba735
leedongkyu <leedongkyu0225@gmail.com>
2025-09-07 15:08:11 +0000

    feat: make client and server script

---
+++ server.sh
@@ -0,0 +1,5 @@
+zsh -c "source ./install/setup.zsh"
+projectDir="./src/action_control_cpp/package.xml"
+result=$(grep "name" $projectDir)
+value=$(echo "$result" | grep -oP '(?<=<name>).*?(?=</name>)')
+zsh -c "ros2 run $value ControlActionServer"
